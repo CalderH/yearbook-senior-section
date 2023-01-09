a952b45e40d3a99477fc1110c95f2503ae9cfb21
CalderH <caldertheperson@me.com>
2023-01-09 12:44:52 -0800

    Started adding settings code

---
+++ settings.py
@@ -0,0 +1,9 @@
+import json
+
+__all__ = ['system_settings', 'year_settings']
+
+with open('settings.json') as system_settings_file:
+    system_settings = json.load(system_settings_file)
+
+with open(system_settings['settings path']) as year_settings_file:
+    year_settings = json.load(year_settings_file)
