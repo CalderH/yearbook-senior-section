51e272831c1d41eb8845f37af7712999e0a73c9c
CalderH <caldertheperson@me.com>
2023-03-12 03:11:51 -0400

    Updated template (but not code) to reflect changes in DB design

--- database.py
+++ database.py
@@ -18,25 +18,35 @@ class Database:
         self.data = None
     
     def setup(self):
-        root_commit_id = convert_id(start_id, ID.c)
+        base_version_id = convert_id(start_id, ID.v)
+        end_version_id = next_id(base_version_id)
         main_branch_id = convert_id(start_id, ID.b)
+
         self.data = JSONDict('database', database_template, {})
-        self.data.root = root_commit_id
+
+        self.data.root = base_version_id
         self.data.branches = {main_branch_id: {}}
         main_branch = self.data.branches[main_branch_id]
         main_branch.name = 'main'
-        main_branch.start = root_commit_id
-        main_branch.end = root_commit_id
+        main_branch.start = base_version_id
+        main_branch.end = end_version_id
         main_branch.open = True
-        self.data.views = {}
         self.data.working_branch = main_branch_id
-        self.data.next_commit_id = next_id(root_commit_id)
+
+        self.data.views = {}
+
+        self.data.next_version_id = next_id(end_version_id)
         self.data.next_branch_id = next_id(main_branch_id)
-        self.data.commits = {root_commit_id: {}}
-        initial_commit = self.data.commits[root_commit_id]
-        initial_commit.message = 'Initial commit'
-        initial_commit.branch = main_branch_id
-        initial_commit.records = {}
+
+        self.data.versions = {base_version_id: {}, end_version_id: {}}
+        base_version = self.data.versions[base_version_id]
+        base_version.message = 'Base'
+        base_version.branch = main_branch_id
+        base_version.next = end_version_id
+
+        end_version = self.data.versions[end_version_id]
+        end_version.branch = main_branch_id
+        end_version.previous = base_version_id
         self.save()
     
     def load(self):
@@ -50,18 +60,6 @@ class Database:
     def check_well_formed(self):
         # TODO
         pass
-
-    def update_open_version(self, deltas, branch_id=None, unchecked=None):
-        if branch_id is None:
-            branch_id = self.data.working_branch
-        branch = self.data.branches[branch_id]
-        if not branch.open:
-            raise YBDBException('Attempted to update the deltas on a committed version')
-        version = self.data.versions[branch.end]
-        version.deltas = deltas
-        if unchecked is not None:
-            version.unchecked = unchecked
-        self.save()
     
     def commit_version(self, branch_id=None):
         if branch_id is None:
@@ -73,19 +71,32 @@ class Database:
         current_version_id = branch.end
         new_version_id = self.data.next_version_id
         current_version = self.data.versions[current_version_id]
-        if current_version.unchecked is not None:
+        if current_version.change is not None and current_version.change.unchecked is not None:
             raise YBDBException('Cannot commit a version with unchecked edits')
         
         self.data.versions[new_version_id] = {}
         new_version = self.data.versions[new_version_id]
         new_version.branch = branch_id
-        new_version.deltas = {}
         current_version.next = new_version_id
         new_version.previous = current_version_id
         branch.end = new_version_id
 
         self.data.next_version_id = next_id(new_version_id)
 
+        self.save()
+    
+    def change_open_version(self, deltas, branch_id=None, unchecked=None):
+        if branch_id is None:
+            branch_id = self.data.working_branch
+        branch = self.data.branches[branch_id]
+        if not branch.open:
+            raise YBDBException('Attempted to update the deltas on a committed version')
+        version = self.data.versions[branch.end]
+        if 'change' not in version:
+            version.change = {}
+        version.change.deltas = deltas
+        if unchecked is not None:
+            version.change.unchecked = unchecked
         self.save()
 
     def new_branch(self, version_id, name):
@@ -105,7 +116,6 @@ class Database:
         new_version = self.data.versions[new_version_id]
         new_version.previous = version_id
         new_version.branch = new_branch_id
-        new_version.deltas = {}
 
         new_branch.end = new_version_id
         new_branch.open = True
@@ -115,5 +125,5 @@ class Database:
         self.data.next_version_id = next_id(self.data.next_version_id)
         self.data.next_branch_id = next_id(self.data.next_branch_id)
     
-    def merge_branches(self, primary_id, secondary_id):
+    def merge_branches(self, primary_id, secondary_ids):
         pass
